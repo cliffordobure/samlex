4110573921a6624fe2fa36c44dc411f68ef1a16a
clifford obure <cliffordobure98@gmail.com>
2026-01-25 09:13:24 -0800

    frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,6 @@ import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 import { store } from "./store/index.js";
 import "./index.css";
-import "./config/forceProduction.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
